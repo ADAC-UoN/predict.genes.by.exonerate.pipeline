010fe4bf3b7ce24537749959a8e1d05b7c4b8723
richarddemes <richard.emes@gmail.com>
2017-04-27 10:48:14 +0100

    catch some weird formatted sequneces on input

--- predict.genes.by.exonerate.pipeline.pl
+++ predict.genes.by.exonerate.pipeline.pl
@@ -62,219 +62,219 @@ print "$usage\nWARNING: Cannot proceed without max number of jobs\n\n"; exit;
 ####################################################
 
 
-
-#filter contigs if too short to hold sequence of interest
-# generate lookup so can parse files easier if long names.
-open SEQLOOKUP, ">$prefix.Target.sequence.lookup.txt";
-open PASSEDTARGETFA, ">$prefix.Target.passed.filter.fa";
-open TMPTABTARGETFA, ">$prefix.Target.passed.filter.tab.tmp";
-
-my $time = scalar localtime;
-print "\n$time Filtering target fasta\n";
-open FASTA, $target_fa;
-my $seq_tally = 1;
-my $passed_count = 0;
-{
-local $/ = '>';
-<FASTA>;                                  # throw away the first line 'cos will only contain ">"
-
-while (<FASTA>) {
-    chomp $_;
-    my ($seq_id, @sequence) = split "\n";            # split the fasta input into Id and sequence
-    my $sequence = join '',@sequence;          # reassembles the sequence
-        my $length = length $sequence;
-
-        if ($length >= 3*$overlap)
-        {
-        print PASSEDTARGETFA "\>$seq_tally\n$sequence\n";
-        print SEQLOOKUP "$seq_tally\t$seq_id\n";
-        print TMPTABTARGETFA "$seq_tally\t$sequence\n";
-        $passed_count++;
-      }
-
-
-$seq_tally++;
-}
-}
-
-
-###################################################################################
-### SPLIT CONTIG FASTA AND RUN EXONERATE
-###################################################################################
-
-
-$time = scalar localtime;
-print "$time Splitting files\n";
-
-# split fasta into multiple files to run exonerate on
-my $maxlines = int(($passed_count/$jobs)+0.5);
-system "split -l $maxlines $prefix.Target.passed.filter.tab.tmp splits --additional-suffix=.tmp";
-
-opendir(TMPFILES, "./");
-my @tmp = sort readdir(TMPFILES);
-closedir(TMPFILES);
-
-
-my @fastain = ();
-my $fasta_count = 1;
-foreach(@tmp)
-        {
-        chomp $_;
-        if(/^splits.*\.tmp$/)
-                {
-                chomp $_;
-                my $filenamein = $_;
-                open FILEIN, $filenamein;
-                open FILEOUT, ">$filenamein\.fa.tmp";
-                while (<FILEIN>){
-                chomp $_;
-                if ($_ =~ /(.*)\t(.*)/)
-                {
-                print FILEOUT "\>$1\n$2\n";
-                }
-                }
-
-                push(@fastain, "$filenamein\.fa.tmp");
-                }
-        }
-@tmp=();
-
-$time = scalar localtime;
-print "$time Starting Exonerate using nohup to spawn multiple jobs\n";
-
-my @tmpexonerate = ();
-foreach (@fastain)
-	{
-	chomp $_;
-	my $filename = $_;
-  my $output = "$filename\.ex\.tmp";
-  push (@tmpexonerate, $output);
-  system "nohup exonerate --model protein2genome --ryo \">%ti (%tab - %tae)\n%tcs\n\" --showcigar no --showquerygff no --showvulgar no --showalignment yes --useaatla no $seq $filename > $output &";
-  }
-
-my $finished_flag = 0;
-my $exonerate_complete = 0;
-
-while ($finished_flag == 0)
-  {
-  system "tail -n1 *.ex.tmp > tails.tmp";
-  open TAILSTMP, "<tails.tmp";
-  while (<TAILSTMP>)
-    {
-    chomp $_;
-    if ($_ =~ /^-- completed exonerate analysis/) {$exonerate_complete++;}
-    }
-  close TAILSTMP;
-
-  if ($exonerate_complete < $jobs) {sleep 60; $exonerate_complete=0;}
-  elsif ($exonerate_complete == $jobs)
-        {
-        $finished_flag = 1;
-        print "\n";
-        $time = scalar localtime;
-        print "$time Finished Exonerate\n";
-        }
-  }
-
-# join exonerate files
-foreach (@tmpexonerate)
-  {
-  chomp $_;
-  system "cat $_ >> $prefix.ex";
-  }
-# clean up a bit
-system "rm ./*.tmp";
-
-###################################################################################
-### PARSE EXOENERATE FILES
-###################################################################################
-# parse exonerate files
-$time = scalar localtime;
-print "$time Parsing Exonerate\n";
-
-open FILE, "$prefix.ex";
-open OUT, ">$prefix.ex.parsed";
-print OUT "Query\tTarget\tscore\tquery_start\tquery_end\tquery_length\ttarget_start\ttarget_end\ttarget_length\tStrand\tIntrons\n";
-
-my $query = "N_A";
-my $target = "N_A";
-my $score = "N_A";
-my $t_end = "";
-my $t_start = "";
-my $q_end = "";
-my $q_start = "";
-my $strand = "";
-my $query_length = 0;
-my $target_length = 0;
-my $intron_count = 0;
-my $flag_hit = 0;
-while (<FILE>)
-{
-chomp $_;
-if ($_ =~ /^C4/){}
-elsif ($_ =~ /^----/){}
-elsif ($_ =~ /^-- completed exonerate analysis/) {}
-elsif ($_ =~ /^Command line\:/){}
-elsif ($_ =~ /^Hostname/){}
-
-
-elsif ($_ =~ /^\s+Query\:\s+(.*)/){$query = $1;  $query =~ s/\s+$//;}
-elsif ($_ =~ /^\s+Target\:\s+(.*)/){$target = $1; $target =~ s/\s+$//;$target =~ s/\s+\[revcomp\]//;}
-elsif ($_ =~ /^\s+Raw score\:\s+(\d+)/) {$score = $1}
-elsif ($_ =~ /^\s+Query range\:\s+(\d+)\s+\-\>\s+(\d+)\s*/)
-		{
-		$q_start = $1;
-		$q_end = $2;
-		$query_length = ($q_end - $q_start);
-		}
-elsif ($_ =~ /^\s+Target range\:\s+(\d+)\s+\-\>\s+(\d+)\s*/)
-		{
-		$t_start = $1;
-		$t_end = $2;
-		$target_length = ($t_end - $t_start);
-		if
-		($t_end < $t_start)
-			{
-			$strand = "-";
-			my $left = $t_end;
-			my $right = $t_start;
-			$t_end = $right;
-			$t_start = $left;
-			$target_length = ($t_end - $t_start);
-			}
-		else
-		{
-		$strand = "+";
-		}
-
-		}
-elsif ($_ =~ /> T/ || /\:\s+Targ/)
-	{
-	$intron_count++;
-	}
-
-elsif ($_ =~ /^\>/)
-	{
-	print OUT "$query\t$target\t$score\t";
-	print OUT "$q_start\t$q_end\t$query_length\t$t_start\t$t_end\t$target_length\t$strand\t$intron_count\n";
-	$query = "N_A"; $score = "N_A";
-	$intron_count = 0;
-	}
-
-
-else {}
-}
-
-
-
-$time = scalar localtime;
-print "$time Finished Parsing Exonerate\n";
-# #
+#
+# #filter contigs if too short to hold sequence of interest
+# # generate lookup so can parse files easier if long names.
+# open SEQLOOKUP, ">$prefix.Target.sequence.lookup.txt";
+# open PASSEDTARGETFA, ">$prefix.Target.passed.filter.fa";
+# open TMPTABTARGETFA, ">$prefix.Target.passed.filter.tab.tmp";
+#
+# my $time = scalar localtime;
+# print "\n$time Filtering target fasta\n";
+# open FASTA, $target_fa;
+# my $seq_tally = 1;
+# my $passed_count = 0;
+# {
+# local $/ = '>';
+# <FASTA>;                                  # throw away the first line 'cos will only contain ">"
+#
+# while (<FASTA>) {
+#     chomp $_;
+#     my ($seq_id, @sequence) = split "\n";            # split the fasta input into Id and sequence
+#     my $sequence = join '',@sequence;          # reassembles the sequence
+#         my $length = length $sequence;
+#
+#         if ($length >= 3*$overlap)
+#         {
+#         print PASSEDTARGETFA "\>$seq_tally\n$sequence\n";
+#         print SEQLOOKUP "$seq_tally\t$seq_id\n";
+#         print TMPTABTARGETFA "$seq_tally\t$sequence\n";
+#         $passed_count++;
+#       }
+#
+#
+# $seq_tally++;
+# }
+# }
+#
+#
+# ###################################################################################
+# ### SPLIT CONTIG FASTA AND RUN EXONERATE
+# ###################################################################################
+#
+#
+# $time = scalar localtime;
+# print "$time Splitting files\n";
+#
+# # split fasta into multiple files to run exonerate on
+# my $maxlines = int(($passed_count/$jobs)+0.5);
+# system "split -l $maxlines $prefix.Target.passed.filter.tab.tmp splits --additional-suffix=.tmp";
+#
+# opendir(TMPFILES, "./");
+# my @tmp = sort readdir(TMPFILES);
+# closedir(TMPFILES);
+#
+#
+# my @fastain = ();
+# my $fasta_count = 1;
+# foreach(@tmp)
+#         {
+#         chomp $_;
+#         if(/^splits.*\.tmp$/)
+#                 {
+#                 chomp $_;
+#                 my $filenamein = $_;
+#                 open FILEIN, $filenamein;
+#                 open FILEOUT, ">$filenamein\.fa.tmp";
+#                 while (<FILEIN>){
+#                 chomp $_;
+#                 if ($_ =~ /(.*)\t(.*)/)
+#                 {
+#                 print FILEOUT "\>$1\n$2\n";
+#                 }
+#                 }
+#
+#                 push(@fastain, "$filenamein\.fa.tmp");
+#                 }
+#         }
+# @tmp=();
+#
+# $time = scalar localtime;
+# print "$time Starting Exonerate using nohup to spawn multiple jobs\n";
+#
+# my @tmpexonerate = ();
+# foreach (@fastain)
+# 	{
+# 	chomp $_;
+# 	my $filename = $_;
+#   my $output = "$filename\.ex\.tmp";
+#   push (@tmpexonerate, $output);
+#   system "nohup exonerate --model protein2genome --ryo \">%ti (%tab - %tae)\n%tcs\n\" --showcigar no --showquerygff no --showvulgar no --showalignment yes --useaatla no $seq $filename > $output &";
+#   }
+#
+# my $finished_flag = 0;
+# my $exonerate_complete = 0;
+#
+# while ($finished_flag == 0)
+#   {
+#   system "tail -n1 *.ex.tmp > tails.tmp";
+#   open TAILSTMP, "<tails.tmp";
+#   while (<TAILSTMP>)
+#     {
+#     chomp $_;
+#     if ($_ =~ /^-- completed exonerate analysis/) {$exonerate_complete++;}
+#     }
+#   close TAILSTMP;
+#
+#   if ($exonerate_complete < $jobs) {sleep 60; $exonerate_complete=0;}
+#   elsif ($exonerate_complete == $jobs)
+#         {
+#         $finished_flag = 1;
+#         print "\n";
+#         $time = scalar localtime;
+#         print "$time Finished Exonerate\n";
+#         }
+#   }
+#
+# # join exonerate files
+# foreach (@tmpexonerate)
+#   {
+#   chomp $_;
+#   system "cat $_ >> $prefix.ex";
+#   }
+# # clean up a bit
+# system "rm ./*.tmp";
+#
+# ###################################################################################
+# ### PARSE EXOENERATE FILES
+# ###################################################################################
+# # parse exonerate files
+# $time = scalar localtime;
+# print "$time Parsing Exonerate\n";
+#
+# open FILE, "$prefix.ex";
+# open OUT, ">$prefix.ex.parsed";
+# print OUT "Query\tTarget\tscore\tquery_start\tquery_end\tquery_length\ttarget_start\ttarget_end\ttarget_length\tStrand\tIntrons\n";
+#
+# my $query = "N_A";
+# my $target = "N_A";
+# my $score = "N_A";
+# my $t_end = "";
+# my $t_start = "";
+# my $q_end = "";
+# my $q_start = "";
+# my $strand = "";
+# my $query_length = 0;
+# my $target_length = 0;
+# my $intron_count = 0;
+# my $flag_hit = 0;
+# while (<FILE>)
+# {
+# chomp $_;
+# if ($_ =~ /^C4/){}
+# elsif ($_ =~ /^----/){}
+# elsif ($_ =~ /^-- completed exonerate analysis/) {}
+# elsif ($_ =~ /^Command line\:/){}
+# elsif ($_ =~ /^Hostname/){}
+#
+#
+# elsif ($_ =~ /^\s+Query\:\s+(.*)/){$query = $1;  $query =~ s/\s+$//;}
+# elsif ($_ =~ /^\s+Target\:\s+(.*)/){$target = $1; $target =~ s/\s+$//;$target =~ s/\s+\[revcomp\]//;}
+# elsif ($_ =~ /^\s+Raw score\:\s+(\d+)/) {$score = $1}
+# elsif ($_ =~ /^\s+Query range\:\s+(\d+)\s+\-\>\s+(\d+)\s*/)
+# 		{
+# 		$q_start = $1;
+# 		$q_end = $2;
+# 		$query_length = ($q_end - $q_start);
+# 		}
+# elsif ($_ =~ /^\s+Target range\:\s+(\d+)\s+\-\>\s+(\d+)\s*/)
+# 		{
+# 		$t_start = $1;
+# 		$t_end = $2;
+# 		$target_length = ($t_end - $t_start);
+# 		if
+# 		($t_end < $t_start)
+# 			{
+# 			$strand = "-";
+# 			my $left = $t_end;
+# 			my $right = $t_start;
+# 			$t_end = $right;
+# 			$t_start = $left;
+# 			$target_length = ($t_end - $t_start);
+# 			}
+# 		else
+# 		{
+# 		$strand = "+";
+# 		}
+#
+# 		}
+# elsif ($_ =~ /> T/ || /\:\s+Targ/)
+# 	{
+# 	$intron_count++;
+# 	}
+#
+# elsif ($_ =~ /^\>/)
+# 	{
+# 	print OUT "$query\t$target\t$score\t";
+# 	print OUT "$q_start\t$q_end\t$query_length\t$t_start\t$t_end\t$target_length\t$strand\t$intron_count\n";
+# 	$query = "N_A"; $score = "N_A";
+# 	$intron_count = 0;
+# 	}
+#
+#
+# else {}
+# }
+#
+#
+#
+# $time = scalar localtime;
+# print "$time Finished Parsing Exonerate\n";
+# # #
 ###################################################################################
 ### MERGE OVERLAPPING AND FIND NON OVERLAPPING BEST HITS BASED ON EXOENERATE SCORES
 ###################################################################################
 
-$time = scalar localtime;
+my $time = scalar localtime;
 print "$time Finding non-overlapping best hits\n";
 
 open PARSED, "<$prefix.ex.parsed";
@@ -292,13 +292,15 @@ while (<PARSED>)
     my $line = $_;
     my @data = split '\t', $line;
     my $contig = $data[1];
-    if ($data[5] >= $overlap && $data[10] <= $max_introns)
+    if ($data[5] =~ /^\d/ && $data[10] =~ /^\d/)
       {
-      push (@contigswithhits, $contig);
-      push(@{$contiglookup{$contig}},$line);
+      if ($data[5] >= $overlap && $data[10] <= $max_introns)
+        {
+        push (@contigswithhits, $contig);
+        push(@{$contiglookup{$contig}},$line);
+        }
       }
-  }
-
+    }
 
 my @uniqcontigswithhits = uniq_array(@contigswithhits);
 
@@ -442,15 +444,12 @@ while (<COMBBEST>)
   }
 
 
-$time = scalar localtime;
-print "$time Finished Finding non-overlapping predicted genes\n";
-
 ###################################################################################
 ### PARSE FASTA FROM EXONERATE FILE
 ###################################################################################
 
 $time = scalar localtime;
-print "$time Create Fasta file of predicted genes\n";
+print "$time Creating Fasta file of predicted genes\n";
 
 system "grep -v '^ ' $prefix.ex |grep -v '^\$' |grep -v '^-' | grep -v '^C4' | grep -v '^Host' |grep -v '^Command' | grep -v '^%' > $prefix.ex.fasta.tmp";
 
@@ -472,7 +471,9 @@ while (<ALLFASTA>)
 
 system "sort $prefix.ex.tab.tmp \| uniq > $prefix.ex.tab.uniq.tmp";
 
-foreach (@nonoverlapping_lookup)
+my @nonoverlapping_lookup_uniq = uniq_array(@nonoverlapping_lookup);
+
+foreach (@nonoverlapping_lookup_uniq)
 {
 chomp $_;
 system "grep \"$_\" $prefix.ex.tab.uniq.tmp >> $prefix.ex.tab.uniq.keep.tmp";
@@ -480,12 +481,13 @@ system "grep \"$_\" $prefix.ex.tab.uniq.tmp >> $prefix.ex.tab.uniq.keep.tmp";
 
 
 open KEEPTABIN, "$prefix.ex.tab.uniq.keep.tmp";
-open KEEPFASTAOUT, ">$prefix.ex.nonoverlapping.predictions.fasta";
+open KEEPFASTAOUT, ">$prefix.ex.nonoverlapping.All.predictions.fasta";
 while (<KEEPTABIN>){
         chomp $_;
         my $line = $_;
         if ($line =~ /(.*)\t(.*)/)
-                {print KEEPFASTAOUT "\>$prefixlookup{$1} $1\n$2\n";
+                {
+                print KEEPFASTAOUT "\>$prefixlookup{$1} $1\n$2\n";
                 }
         }
 
